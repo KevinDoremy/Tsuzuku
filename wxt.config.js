24de23dcd85e0132f40d83e329bbe3736cf90085
KevinDoremy <kevin.doremy-laferriere.1@ens.etsmtl.ca>
2026-03-15 23:46:26 -0400

    Set up WXT build tooling

---
+++ wxt.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'wxt';
+
+export default defineConfig({
+  manifest: {
+    name: 'Tsuzuku',
+    description: 'Track your anime progress on Franime and Crunchyroll.',
+    permissions: ['storage'],
+    icons: {
+      16: '/icon16.png',
+      48: '/icon48.png',
+      128: '/icon128.png',
+    },
+  },
+});
